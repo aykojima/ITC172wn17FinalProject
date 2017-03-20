10b6a631b3a1ce3dcf3472d044633dde8a733461
aykojima <ayumikjm@gmail.com>
2017-03-20 12:00:44 -0700

    Initial Commit
    Only added Controllers, Models, and Views files due to the size.

---
+++ Controllers/DonateController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    public class DonateController : Controller
+    {
+        Community_AssistEntities db = new Community_AssistEntities();
+        // GET: Donate
+        public ActionResult Index()
+        {
+            var donation = from d in db.Donation
+                         select new
+                         {
+                             d.DonationKey,
+                             d.Person.PersonFirstName,
+                             d.Person.PersonLastName,
+                             d.Person.PersonEmail,
+                             d.DonationAmount
+                         };
+            List<Donate> donate = new List<Donate>();
+            foreach (var d in donation)
+            {
+                Donate dnt = new Donate();
+                dnt.DonationKey = d.DonationKey;
+                dnt.PersonFirstName = d.PersonFirstName;
+                dnt.PersonLastName = d.PersonLastName;
+                dnt.PersonEmail = d.PersonEmail;
+                dnt.DonationAmount = d.DonationAmount;
+                donate.Add(dnt);
+
+            }
+            return View(donate);
+        }
+    }
+}
---
+++ Models/Donate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.Models
+{
+    public class Donate
+    {
+        public string PersonLastName { get; set; }
+        public string PersonFirstName { get; set; }
+        public string PersonEmail { get; set; }
+        public decimal DonationAmount { get; set; }
+        public int DonationKey { get; set; }
+
+    }
+}
